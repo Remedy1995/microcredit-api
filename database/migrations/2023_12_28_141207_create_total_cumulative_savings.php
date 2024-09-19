84105871a9f54e611c6a1686395bf38b7f571cd5
Remedy1995 <adjetadjetey45@gmail.com>
2024-09-19 14:12:39 +0000

    updated nginx conf supervisor php fpm

--- database/migrations/2023_12_28_141207_create_total_cumulative_savings.php
+++ database/migrations/2023_12_28_141207_create_total_cumulative_savings.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('total_cumulative_savings', function (Blueprint $table) {
             $table->id();
-            $table->float('total_cumulative_savings')->default(0.0);
+            $table->doube('total_cumulative_savings',15,2)->change();
             $table->timestamps();
         });
     }
